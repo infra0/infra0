0ed8a074a18f1f23e497c6d7b6dacceb5c3f1eab
ashu565 <ashu9993063472@gmail.com>
2025-07-25 16:39:50 +0530

    Fixed the config to avoid the infinite compiling

--- visualizer/ui/next.config.mjs
+++ visualizer/ui/next.config.mjs
@@ -1,41 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: false,
   },
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: false,
   },
   images: {
     unoptimized: true,
   },
-  distDir: 'build',
-
-  // Add Webpack and watch logging
-  webpack(config, { dev }) {
-    if (dev) {
-      console.log('[DEBUG] Webpack config is running in development mode');
-
-      config.plugins.push({
-        apply: (compiler) => {
-          compiler.hooks.watchRun.tap('WatchLoggerPlugin', (comp) => {
-            console.log('[WATCH RUN] Detected file change triggering rebuild');
-          });
-        }
-      });
-    }
-    return config;
-  },
-
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      ...config.watchOptions,
-      ignored: ['**/.next/**', '**/node_modules/**', '**/build/**', '**/public/**'],
-    };
-
-    console.log('[DEBUG] webpackDevMiddleware watchOptions:', config.watchOptions);
-    return config;
-  }
 };
 
 export default nextConfig;
