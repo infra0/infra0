443821262abece8d289637959aae21d8dd45a5b3
ashu565 <ashu9993063472@gmail.com>
2025-07-25 15:01:15 +0530

    did the backend error handling

--- visualizer/server/config/index.ts
+++ visualizer/server/config/index.ts
@@ -9,6 +9,7 @@ const envpath = process.env.NODE_ENV === 'development' ? `${__dirname}/../.env.d
 dotenv.config({ path: envpath });
 
 export const config = {
+  nodeEnv: process.env.NODE_ENV || 'development',
   jwtSecret: process.env.JWT_SECRET || 'infra0-jwt-secret-1945',
   mongoDbUri: process.env.MONGO_DB_URI || 'mongodb://localhost:27017/visualizer',
   anthropicApiKey: process.env.ANTHROPIC_API_KEY,
